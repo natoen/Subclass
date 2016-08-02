b623bec4bd5f6b485ff1bcd2ec7e51b6cbda974e
natoen <hgmatsuda@gmail.com>
2016-08-03 08:23:14 +0900

    (feat) Add new lineUp functionality for saraDancer

--- src/saraDancer.js
+++ src/saraDancer.js
@@ -1,11 +1,21 @@
-var saraDancer = function(top, left, timeBetweenSteps, dancerName){
+var saraDancer = function(top, left, timeBetweenSteps, dancerName) {
   makeDancer.call(this, top, left, timeBetweenSteps, dancerName);
 };
 
 saraDancer.prototype = Object.create(makeDancer.prototype);
 saraDancer.prototype.constructor = saraDancer;
 
-saraDancer.prototype.step = function(){
+saraDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
   this.$node.toggle();
+};
+
+saraDancer.prototype.lineUp = function(top, left) {
+  makeDancer.prototype.lineUp.call(this, top, left);
+  var top = this.$node.parent().height() * Math.random();
+  var left = this.$node.parent().width() * Math.random();
+
+  setTimeout(function() {
+    this.setPosition(top, left);
+  }.bind(this), 2000);
 };
